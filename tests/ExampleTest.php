6b25517bb1626423bb1c25ecb26da8f0eb4f076c
cmosguy <asklein@gmail.com>
2016-07-24 21:18:42 -0700

    Initial commit with my test for :memory: for sqlite in phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->visit('/')
+             ->see('Laravel 5');
+    }
+}
